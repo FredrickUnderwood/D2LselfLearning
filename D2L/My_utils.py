cb68013462508bb285407f7db04d2d11ddfd7415
FredrickUnderwood <1519995555@qq.com>
2024-09-11 21:09:00 +0800

    update gitignore

--- D2L/My_utils.py
+++ D2L/My_utils.py
@@ -14,6 +14,7 @@ import os
 import collections
 import random
 import re
+import math
 
 
 # 生成人工数据集
@@ -1044,3 +1045,133 @@ class SeqDataLoader:
 def load_data_time_machine(batch_size, num_steps, use_random_iter=False, max_tokens=10000):
     data_iter = SeqDataLoader(batch_size, num_steps, use_random_iter, max_tokens)
     return data_iter, data_iter.vocab
+
+
+# RNN初始化参数
+def get_params(vocab_size, num_hiddens, device):
+    # 输入和输出采用相同的词表，vocab_size是使用的词表的大小
+    # 所以这实际上是一个多分类的问题
+    num_inputs = num_outputs = vocab_size
+
+    def normal(shape):
+        return torch.randn(size=shape, device=device) * 0.01
+
+    w_xh = normal((num_inputs, num_hiddens))
+    w_hh = normal((num_hiddens, num_hiddens))
+    b_h = torch.zeros(num_hiddens, device=device)
+    w_hq = normal((num_hiddens, num_outputs))
+    b_q = torch.zeros(num_outputs, device=device)
+    params = [w_xh, w_hh, b_h, w_hq, b_q]
+    for param in params:
+        param.requires_grad_(True)
+    return params
+
+
+# RNN初始化函数
+def init_rnn_state(batch_size, num_hiddens, device):
+    return (torch.zeros((batch_size, num_hiddens), device=device), )
+
+
+# RNN前向传播函数
+def rnn(inputs, state, params):
+    w_xh, w_hh, b_h, w_hq, b_q = params
+    h, = state
+    outputs = []
+    for x in inputs:
+        h = torch.tanh(torch.mm(x, w_xh) + torch.mm(h, w_hh)+ b_h)  # h: batch_size * num_hiddens
+        y = torch.mm(h, w_hq) + b_q
+        outputs.append(y)
+    # outputs是一个存tensor的list
+    return torch.cat(outputs, dim=0), (h,)
+
+
+# RNN模型
+class RNNModelScratch:
+    def __init__(self, vocab_size, num_hiddens, device, get_params, init_state, forward_fn):
+        self.vocab_size, self.num_hiddens = vocab_size, num_hiddens
+        self.params = get_params(vocab_size, num_hiddens, device)  # 用于初始化模型参数
+        self.init_state, self.forward_fn = init_state, forward_fn
+
+    def __call__(self, x, state):  # x: batch_size * num_steps
+        x = F.one_hot(x.T, self.vocab_size).type(torch.float32)  # (35, 32, 28)
+        return self.forward_fn(x, state, self.params)
+
+    def begin_state(self, batch_size, device):
+        return self.init_state(batch_size, self.num_hiddens, device)
+
+
+# 序列模型预测
+def predict_seq(prefix, num_preds, net, vocab, device):
+    # 生成prefix之后的字符
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]  # text -> num
+    get_input = lambda: torch.tensor([outputs[-1]], device=device).reshape((1, 1))
+    # 将prefix读入outputs中
+    for y in prefix[1: ]:
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+    return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+
+# 梯度剪裁
+def grad_clipping(net, theta):
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+# 序列模型单epoch训练
+def train_epoch_seq(net, train_iter, loss, updater, device, use_random_iter):
+    state, timer = None, Timer()
+    metric = Accumulator(2)  # 训练损失之和，词语元数量
+    for x,y in train_iter:
+        if state is None or use_random_iter:  # use_random_iter说明seq不连续
+            state = net.begin_state(batch_size=x.shape[0], device=device)
+        else:
+            if isinstance(net, nn.Module) and not isinstance(state, tuple):
+                state.detach_()
+            else:
+                for s in state:
+                    s.detach_()
+        y = y.T.reshape(-1)  # y.T: num_steps * batch_size
+        x, y = x.to(device), y.to(device)
+        y_hat, state = net(x, state)
+        l = loss(y_hat, y.long()).mean()
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.backward()
+            grad_clipping(net, 1)
+            updater.step()
+        else:
+            l.backward()
+            grad_clipping(net, 1)
+            updater(batch_size=1)
+        metric.add(l * y.numel(), y.numel())
+    return math.exp(metric[0] / metric[1]), metric[1] / timer.stop()
+
+
+# 序列模型训练
+def train_seq(net, train_iter, vocab, lr, num_epochs, device, use_random_iter=False):
+    loss = nn.CrossEntropyLoss()
+    animator = Animator(xlabel='epoch', ylabel='perplexity', legend=['train'], xlim=[10, num_epochs])
+    if isinstance(net, nn.Module):
+        updater = torch.optim.SGD(net.parameters(), lr=lr)
+    else:
+        updater = lambda batch_size: sgd(net.params, lr, batch_size)
+    predict = lambda prefix: predict_seq(prefix, 50, net, vocab, device)
+    for epoch in range(num_epochs):
+        ppl, speed = train_epoch_seq(net, train_iter, loss, updater, device, use_random_iter)
+        if (epoch + 1) % 10 == 0:
+            print(predict('time traveller'))
+            animator.add(epoch + 1, [ppl])
+    print(f'perplexity: {ppl:.1f}, speed: {speed:.1f} words/sec on {str(device)}')
+    print(predict('time traveller'))
+    print(predict('traveller'))
